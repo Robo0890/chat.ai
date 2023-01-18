ce877f5a304e671d446a6d1da28c574edbbd6018
Ryan <70237244+Robo0890@users.noreply.github.com>
2023-01-18 16:54:58 -0600

    Finished

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1674022088|4513578266";
+const CACHE_VERSION = "1674082485|1295766029";
 const CACHE_PREFIX = "Interview Projec-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
