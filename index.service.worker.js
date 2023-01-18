41a1ee897ef7ed243f4c36ec2f19457dda6bd3de
Ryan <70237244+Robo0890@users.noreply.github.com>
2023-01-17 23:52:16 -0600

    Finish Tuning

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1674667420|20778330";
+const CACHE_VERSION = "1674021127|3553118236";
 const CACHE_PREFIX = "Interview Projec-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
