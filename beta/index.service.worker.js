b8dc371963502b33034484f9a34c1339bbe18423
Ryan <70237244+Robo0890@users.noreply.github.com>
2023-01-18 18:40:17 -0600

    Keyboard

--- beta/index.service.worker.js
+++ beta/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1674088621|802108518";
+const CACHE_VERSION = "1674088805|985411487";
 const CACHE_PREFIX = "Interview Projec-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
